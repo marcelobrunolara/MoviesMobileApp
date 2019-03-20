aab71cec257ae4e5552b555a8464f0b08db2807f
Marcelo Lara <marcelobrunolara@gmail.com>
2019-03-20 19:00:18 -0300

    Ajuste em arquivos devido a conflito de commits.

--- src/MoviesMobileApp/MoviesMobileApp/MoviesMobileApp/ViewModels/Input/MovieViewModel.cs
+++ src/MoviesMobileApp/MoviesMobileApp/MoviesMobileApp/ViewModels/Input/MovieViewModel.cs
@@ -1,8 +1,16 @@
 ﻿using ML.Framework.Base.MVVM.Input;
+using System;
 
 namespace MoviesMobileApp.ViewModels.Input
 {
     public class MovieViewModel : BaseViewModel
     {
+        public string Name { get; set; }
+        public string PosterUrl { get; set; }
+        public string BackdropUrl { get; set; }
+        public string Genre { get; set; }
+        public string ProductionCompany { get; set; }
+        public string Overview {get;set;}
+        public DateTime ReleaseDate { get; set; }
     }
 }
